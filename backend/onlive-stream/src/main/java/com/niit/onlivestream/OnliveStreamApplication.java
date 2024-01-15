b94a2b2aac7eb1a2d4d155da0837f6686252002b
Arthur-Evans <76278246+Arthur-Evans@users.noreply.github.com>
2024-01-15 14:37:37 +0800

    后端项目创建
    完成后端初始化

---
+++ backend/onlive-stream/src/main/java/com/niit/onlivestream/OnliveStreamApplication.java
@@ -0,0 +1,13 @@
+package com.niit.onlivestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnliveStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnliveStreamApplication.class, args);
+	}
+
+}

524c437c47f2046e00f87e153441936c3f568d14
mandycon <mandycon@icloud.com>
2024-02-02 11:41:05 +0800

    Update

--- frontend/online/src/components/my-info.vue
+++ frontend/online/src/components/my-info.vue
@@ -1,9 +1,33 @@
 <script lang="ts" setup>
 import {login} from "../global/global";
 import User from "../global/vo/User";
+import {ref} from "vue";
 
-let modifyUser = login.user;
+let modifyUser = ref(new User());
 
+const preModifyInfo = () => {
+  const displayElement = document.getElementsByClassName('display')
+  const modifyElement = document.getElementsByClassName('modify')
+  for (let i = 0; i < displayElement.length; i ++) {
+    displayElement[i].style.display='none';
+  }
+
+  for (let i = 0; i < modifyElement.length; i ++) {
+    modifyElement[i].style.display='block'
+  }
+}
+
+const cancelModifyInfo = () => {
+  const displayElement = document.getElementsByClassName('display')
+  const modifyElement = document.getElementsByClassName('modify')
+  for (let i = 0; i < displayElement.length; i ++) {
+    displayElement[i].style.display='block';
+  }
+
+  for (let i = 0; i < modifyElement.length; i ++) {
+    modifyElement[i].style.display='none'
+  }
+}
 </script>
 
 <template>
@@ -64,12 +88,12 @@ let modifyUser = login.user;
 
         <div class="modify">
           <span class="attribute">uuid</span>:
-          <span class="value"> {{ modifyUser.uuid }}</span>
+          <span class="value"> {{ login.user.uuid }}</span>
         </div>
 
         <div class="modify">
           <span class="attribute">用户名</span>:
-          <span class="value"> {{ modifyUser.userAccount }}</span>
+          <span class="value"> {{ login.user.userAccount }}</span>
         </div>
 
         <div class="modify">
@@ -82,17 +106,17 @@ let modifyUser = login.user;
 
         <div class="modify">
           <span class="attribute">年龄</span>:
-          <el-input v-model="modifyUser.userAge" class="value input"></el-input>
+          <el-input v-model="modifyUser._userAge" class="value input"></el-input>
         </div>
 
         <div class="modify">
           <span class="attribute">邮箱</span>:
-          <el-input v-model="modifyUser.userEmail" class="value input"></el-input>
+          <el-input v-model="modifyUser._userEmail" class="value input"></el-input>
         </div>
 
         <div class="modify">
           <span class="attribute">个性签名</span>:
-          <el-input v-model="modifyUser.userSignature" class="value input"></el-input>
+          <el-input v-model="modifyUser._userSignature" class="value input"></el-input>
         </div>
 
       </div>
@@ -111,8 +135,13 @@ let modifyUser = login.user;
       <!--      constraint userAccount-->
       <!--      unique (userAccount)-->
       <!--      );-->
-
-
+      <div style="margin-left: 100px; margin-top: 20px" class="display">
+        <el-button type="primary" @click="preModifyInfo">修改信息</el-button>
+      </div>
+      <div style="margin-left: 100px; margin-top: 20px" class="modify">
+        <el-button type="success">确认修改</el-button>
+        <el-button type="danger" @click="cancelModifyInfo">取消</el-button>
+      </div>
     </div>
   </div>
 </template>
@@ -124,6 +153,7 @@ let modifyUser = login.user;
 
 .modify {
   line-height: 2.5;
+  display: none;
 }
 
 .input {
